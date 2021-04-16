58343a2df01ccf063f7face4dd428afd36321fb3
marcoswb <marcos_wb@outlook.com>
2021-04-16 20:54:22 -0300

    finish style home page

--- personal-site/src/components/MainContent/index.js
+++ personal-site/src/components/MainContent/index.js
@@ -5,6 +5,12 @@ import github from '../../resource/logo/github.png'
 import email from '../../resource/logo/email.png'
 import javacript from '../../resource/logo/javascript.png'
 import java from '../../resource/logo/java.png'
+import perl from '../../resource/logo/perl.png'
+import SQL from '../../resource/logo/SQL.png'
+import AWK from '../../resource/logo/AWK.png'
+import batch from '../../resource/logo/batch.png'
+import react from '../../resource/logo/react.png'
+import git from '../../resource/logo/git.png'
 
 import './style.css'
 
@@ -22,56 +28,66 @@ export default function MainContent(){
                 </div>
                 <div class="find-skills">
                     <div>
-                        <h3>Encontre-me</h3>
-                        <div class="item">
+                        <h3>Skills</h3>
+                        <div class="item-skill">
+                            <div class="item">
+                                <span>Java</span>
+                                <img src={java} alt="logo java"/>
+                            </div>
+                            <div class="item">
+                                <span>JavaScript</span>
+                                <img src={javacript} alt="logo javascrip"/>
+                            </div>
+                        </div>
+                        <div class="item-skill">
+                            <div class="item">
+                                <span>React</span>
+                                <img src={react} alt="logo React"/>
+                            </div>
+                            <div class="item">
+                                <span>SQL</span>
+                                <img src={SQL} alt="logo SQL"/>
+                            </div>
+                        </div>
+                        <div class="item-skill">
+                            <div class="item">
+                                <span>Git</span>
+                                <img src={git} alt="logo git"/>
+                            </div>
+                            <div class="item">
+                                <span>Perl</span>
+                                <img src={perl} alt="logo perl"/>
+                            </div>
+                        </div>
+                        <div class="item-skill">
+                            <div class="item">
+                                <span>AWK</span>
+                                <img src={AWK} alt="logo AWK"/>
+                            </div>
+                            <div class="item">
+                                <span>Batch Script</span>
+                                <img src={batch} alt="logo Batch Script"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="item-findme">
+                            <h3>Encontre-me</h3>
+                        </div>
+                        <div class="item-findme">
                             <a href="https://www.linkedin.com/in/marcos-warmling/" target="_blank">
                                 <img src={linkedin} alt="logo linkedin" class="icons"/>
                             </a>
                         </div>
-                        <div class="item">
+                        <div class="item-findme">
                             <a href="https://www.github.com/marcoswb" target="_blank">
                                 <img src={github} alt="logo github" class="icons"/>
                             </a>
                         </div>
-                        <div class="item">
+                        <div class="item-findme">
                             <a href="#" target="_blank">
                                 <img src={email} alt="logo email" class="icons"/>
                             </a>
-                        </div>
-                    </div>
-                    <div>
-                        <h3>Skills</h3>
-                        <div class="item">
-                            <span>JavaScript</span>
-                            <img src={javacript} alt="logo javascrip"/>
-                        </div>
-                        <div class="item">
-                            <span>Java</span>
-                            <img src={java} alt="logo java"/>
-                        </div>
-                        <div class="item">
-                            <span>Perl</span>
-                            <img src={javacript} alt="logo perl"/>
-                        </div>
-                        <div class="item">
-                            <span>SQL</span>
-                            <img src={javacript} alt="logo SQL"/>
-                        </div>
-                        <div class="item">
-                            <span>AWK</span>
-                            <img src={javacript} alt="logo AWK"/>
-                        </div>
-                        <div class="item">
-                            <span>Batch Script</span>
-                            <img src={javacript} alt="logo Batch Script"/>
-                        </div>
-                        <div class="item">
-                            <span>React</span>
-                            <img src={javacript} alt="logo React"/>
-                        </div>
-                        <div class="item">
-                            <span>Git</span>
-                            <img src={javacript} alt="logo git"/>
                         </div>
                     </div>
                 </div>
