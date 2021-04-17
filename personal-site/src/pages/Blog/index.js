2fb05f31a9cc299157e1bb31762c35bfaede0879
marcoswb <marcos_wb@outlook.com>
2021-04-17 10:43:47 -0300

    finish design

--- personal-site/src/pages/Blog/index.js
+++ personal-site/src/pages/Blog/index.js
@@ -8,7 +8,21 @@ export default function Blog(){
     return(
         <>
             <Navbar />
-            <h1>Aqui vai ser a pagina de Blog</h1>
+            <div class="blog">
+                <div class="header-posts">
+                    <h2>Blog</h2>
+                    <div>
+                        <input type="text" id="search-input" placeholder="Procure um tema/post" />
+                        <button id="search-button">Pesquisar</button>
+                    </div>
+                </div>
+                <h1>Nenhum post criado até o momento</h1>
+                {/* <div class="post-item">
+                   <h4>EXEMPLO DE POST</h4>
+                   <p>Descrição</p>
+                   <p class="date">22 de março de 2021</p>
+                </div> */}
+            </div>
         </>
     )
 }
