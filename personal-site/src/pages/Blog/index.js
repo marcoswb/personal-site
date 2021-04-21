029a0e58bebc56da74307ed49b834b5d57fdd5d3
marcoswb <marcos_wb@outlook.com>
2021-04-21 19:07:45 -0300

    finish application

--- personal-site/src/pages/Blog/index.js
+++ personal-site/src/pages/Blog/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 import Navbar from '../../components/Navbar'
+import data from '../../resource/blog-posts.json'
 
 import './style.css'
 
 export default function Blog(){
+
+    const [valueFind, setValueFind] = useState('')
+    const [posts, setPosts] = useState([])
+    
+    useEffect(() => {
+        setPosts(data)
+    }, [])
+
+    function handleClick(){
+        setPosts(data)
+
+        findPosts(data)
+    }
+
+    function findPosts(posts){
+        const postsFilter = posts.filter(item => {
+            const valueLowerCase = item.tags.toLowerCase()
+            const valueFindLowerCase = valueFind.toLowerCase()
+            if(valueLowerCase.includes(valueFindLowerCase)){
+                return item
+            }
+        })
+        setPosts(postsFilter)
+    }
 
     return(
         <>
@@ -12,16 +38,26 @@ export default function Blog(){
                 <div className="header-posts">
                     <h2>Blog</h2>
                     <div>
-                        <input type="text" id="search-input" placeholder="Procure um tema/post" />
+                        <input 
+                            type="text" 
+                            id="search-input" 
+                            placeholder="Procure um tema/post"
+                            value={valueFind}
+                            onChange={event => setValueFind(event.target.value)}
+                            onKeyUp={handleClick}
+                        />
                         <button id="search-button">Pesquisar</button>
                     </div>
                 </div>
-                <h1>Nenhum post criado até o momento</h1>
-                {/* <div className="post-item">
-                   <h4>EXEMPLO DE POST</h4>
-                   <p>Descrição</p>
-                   <p className="date">22 de março de 2021</p>
-                </div> */}
+                {posts.map(item => (
+                    <a href={item.link} target="_blank">
+                        <div className="project-item">
+                                <h4>{item.title}</h4>
+                                <p className="description">{item.description}</p>
+                                <p className="techs">{item.techs}</p>
+                        </div>
+                    </a>
+                ))}
             </div>
         </>
     )
